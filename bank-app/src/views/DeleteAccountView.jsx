a0c03ac576593afa726d8564cd0ac28e7ad68b8f
ahmad-compacer <79328161+ahmad-compacer@users.noreply.github.com>
2021-05-31 16:15:16 +0200

    delete request

--- bank-app/src/views/DeleteAccountView.jsx
+++ bank-app/src/views/DeleteAccountView.jsx
@@ -1,27 +1,51 @@
 import { FormControlLabel, IconButton } from "@material-ui/core";
-import { blue } from "@material-ui/core/colors";
-import DeleteIcon from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
+import { Component } from "react";
+import axios from "axios";
 
+class MatDelete extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: [],
+    };
+  }
 
-const MatDelete = ({ index }) => {
-  const handleDeleteClick = () => {
-    // some action
-    
-  };
+  loadAccounts() {
+    const baseURL = process.env.REACT_APP_BASE_URL;
+    let endpoint = baseURL + "/v1/accounts";
+    axios.get(endpoint).then((res) => {
+      const accounts = res.data;
+      this.setState({ accounts });
+    });
+  }
 
-  return (
-    <FormControlLabel
-      control={
-        <IconButton
-          color="secondary"
-          aria-label="add an alarm"
-          onClick={handleDeleteClick}
-        >
-          <DeleteIcon  title="Delete your account" style={{ color: blue[500] }} />
-        </IconButton>
-      }
-    />
-  );
-};
-
-export default MatDelete
+  render() {
+    const handleDeleteClick = (id, e) => {
+      const baseURL = process.env.REACT_APP_BASE_URL;
+      let deleteId = baseURL + "/v1/accounts" + { id };
+      axios.delete(deleteId).then((res) => {
+        console.log(res);
+        console.log(res.data);
+        const accounts = this.state.accounts.filter((item) => item.id !== id);
+        this.setState({ accounts });
+      });
+    };
+    return (
+      <FormControlLabel
+        control={
+          <IconButton
+            color="secondary"
+            aria-label="add an alarm"
+            onClick={handleDeleteClick}
+          >
+            <Button variant="contained" color="secondary">
+              Delete your account
+            </Button>
+          </IconButton>
+        }
+      />
+    );
+  }
+}
+export default MatDelete;
