5781b4d63037ce3bef7f66b4cbca26f1a46fb8e8
ahmad-compacer <79328161+ahmad-compacer@users.noreply.github.com>
2021-05-31 09:45:52 +0200

    retutn table

--- bank-app/src/views/AccountsTableView.jsx
+++ bank-app/src/views/AccountsTableView.jsx
@@ -1,46 +1,23 @@
 import { DataGrid } from "@material-ui/data-grid";
 import React from "react";
-import { FormControlLabel, IconButton } from "@material-ui/core";
-import { blue } from "@material-ui/core/colors";
-import DeleteIcon from "@material-ui/icons/Delete";
-
+import MatDelete from "./DeleteAccountView";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
   { field: "name", headerName: "Name", width: 200 },
   { field: "iBan", headerName: "iBan", width: 400 },
   { field: "balance", headerName: "balance", width: 300 },
-
   {field: "actions", headerName: "Delete your account", sortable: false, width: 180, disableClickEventBubbling: true,
-   renderCell: (params) => {
+    renderCell: (params) => {
       return (
         <div>
           <MatDelete index={params.row.id} />
         </div>
       );
     },
-  }
+  },
 ];
-const MatDelete = ({ index }) => {
-  const handleDeleteClick = () => {
-    // some action
-    
-  };
 
-  return (
-    <FormControlLabel
-      control={
-        <IconButton
-          color="secondary"
-          aria-label="add an alarm"
-          onClick={handleDeleteClick}
-        >
-          <DeleteIcon  title="Put money" style={{ color: blue[500] }} />
-        </IconButton>
-      }
-    />
-  );
-};
 export default function AccountsTableView({ items }) {
   return (
     <div style={{ height: 450, width: "100%" }}>
