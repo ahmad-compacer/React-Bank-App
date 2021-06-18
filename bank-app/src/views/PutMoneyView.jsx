70627f6fadc8769bbb2047771997b1a3e89a33c9
ahmad-compacer <79328161+ahmad-compacer@users.noreply.github.com>
2021-06-18 08:58:39 +0200

    Endversionen

--- bank-app/src/views/PutMoneyView.jsx
+++ bank-app/src/views/PutMoneyView.jsx
@@ -1,21 +1,38 @@
 import { FormControlLabel, IconButton } from "@material-ui/core";
-import { blue } from "@material-ui/core/colors";
-import EuroIcon from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
+import React, { Component } from "react";
+import axios from 'axios'
 
-const MatPut = ({put}) => {
+const MatPut = ({ getID }) => {
+
   const handlePutClick = () => {
-    // some action
+    const enteredName = prompt('Please enter the amount you want to deposit into this account')
+    let path = getID + "/" + enteredName;
+    const baseURL = process.env.REACT_APP_BASE_URL;
+    let endpoint = baseURL + "/v1/accounts/" + path;
+    axios.put(endpoint)
+    .then(res => {
+      if(res.data !=null){
+        alert('Done!');
+        window.location.reload(true);
+      }else{
+        alert('Faild')
+      }
+    })
+    .catch((error) => {
+      if (error.request){
+        alert('Faild')
+    }
+  });
   };
 
   return (
     <FormControlLabel
       control={
-        <IconButton
-          color="secondary"
-          aria-label="add an alarm"
-          onClick={handlePutClick}
-        >
-          <EuroIcon title="Put money" style={{ color: blue[500] }} />
+        <IconButton color="secondary" aria-label="add an alarm">
+          <Button variant="contained" onClick={handlePutClick} color="primary">
+            Put money
+          </Button>
         </IconButton>
       }
     />
