b1eeecb619c7b618672f9af2907129567b8feb1c
ahmad-compacer <79328161+ahmad-compacer@users.noreply.github.com>
2021-06-18 08:58:15 +0200

    Endversionen

--- bank-app/src/views/HomePageView.jsx
+++ bank-app/src/views/HomePageView.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+
+
 class HomePageView extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome!</h1>
-        
+        <Grid align="center">
+          <h1>Welcome in our Application!</h1>
+          <h2> Please choose an option from the list above</h2>
+        </Grid>
       </div>
     );
   }
