ecfc39d328b95af2e1d44cf6855793f67d6dc1fd
ahmad-compacer <79328161+ahmad-compacer@users.noreply.github.com>
2021-06-18 08:56:56 +0200

    Endversionen

--- bank-app/src/views/DeleteAccountView.jsx
+++ bank-app/src/views/DeleteAccountView.jsx
@@ -1,51 +1,42 @@
 import { FormControlLabel, IconButton } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
-import { Component } from "react";
-import axios from "axios";
+import axios from 'axios'
 
-class MatDelete extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      accounts: [],
-    };
-  }
+const MatDelete = ({ getID }) => {
 
-  loadAccounts() {
-    const baseURL = process.env.REACT_APP_BASE_URL;
-    let endpoint = baseURL + "/v1/accounts";
-    axios.get(endpoint).then((res) => {
-      const accounts = res.data;
-      this.setState({ accounts });
-    });
-  }
+  const handleDeleteClick = () => {
+    axios.delete(`http://192.168.233.128:8054/accounts/${getID}`)
+    .then(res => {
+      if(res.data !=null){
+        console.log('account deledet successfully')
+        alert('account deledet successfully');
+        window.location.reload(true);
+      }else{
+        alert('account not deledet')
+      }
+    })
+    .catch((error) => {
+      if (error.request){
+        console.log('account not deledet')
+        alert('account not deledet')
+    }
+  });
+  };
 
-  render() {
-    const handleDeleteClick = (id, e) => {
-      const baseURL = process.env.REACT_APP_BASE_URL;
-      let deleteId = baseURL + "/v1/accounts" + { id };
-      axios.delete(deleteId).then((res) => {
-        console.log(res);
-        console.log(res.data);
-        const accounts = this.state.accounts.filter((item) => item.id !== id);
-        this.setState({ accounts });
-      });
-    };
-    return (
-      <FormControlLabel
-        control={
-          <IconButton
-            color="secondary"
-            aria-label="add an alarm"
-            onClick={handleDeleteClick}
-          >
-            <Button variant="contained" color="secondary">
-              Delete your account
-            </Button>
-          </IconButton>
-        }
-      />
-    );
-  }
-}
+  return (
+    <FormControlLabel
+      control={
+        <IconButton
+          color="secondary"
+          aria-label="add an alarm"
+        >
+          <Button variant="contained" onClick={handleDeleteClick} color="secondary">
+            Delete your account
+          </Button>
+        </IconButton>
+      }
+    />
+  );
+};
+
 export default MatDelete;
