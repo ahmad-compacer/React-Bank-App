7ce80c318cad2d8025ee10e5b33900bee42477e7
ahmad-compacer <79328161+ahmad-compacer@users.noreply.github.com>
2021-06-18 09:00:18 +0200

    Endversionen

--- bank-app/src/Router.jsx
+++ bank-app/src/Router.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HeaderView from "./views/HeaderView";
-import AccountsTableView from "./views/AccountsTableView";
+import AccountsComponent from "./components/AccountsComponent";
 import HomePageView from "./views/HomePageView";
-import PutMoneyView from "./views/PutMoneyView";
 import AddNewAccountView from "./views/AddNewAccountView";
-import DeleteAccountView from "./views/DeleteAccountView";
 import ErrorView from "./views/ErrorView";
-
-
+import LoginViwe from './views/LoginViwe'
+import LogoutViwe from './views/LogoutViwe'
+import AuAuthenticationPath from './components/AuthenticationPath'
 
 class BankApp extends Component {
   render() {
@@ -16,15 +15,15 @@ class BankApp extends Component {
       <div>
         <Router>
           <>
-         
             <HeaderView />
-            
+
             <Switch>
-              <Route path="/all" exact component={AccountsTableView} />
-              <Route path="/home" exact component={HomePageView} />
-              <Route path="/put" exact component={PutMoneyView} />
-              <Route path="/post" exact component={AddNewAccountView} />
-              <Route path="/delete" exact component={DeleteAccountView} />
+              <Route path="/" exact component={LoginViwe} />  
+              <Route path="/login" exact component={LoginViwe} />
+              <AuAuthenticationPath path="/logout" exact component={LogoutViwe} />
+              <AuAuthenticationPath path="/all" exact component={AccountsComponent} />
+              <AuAuthenticationPath path="/home" exact component={HomePageView} />
+              <AuAuthenticationPath path="/post" exact component={AddNewAccountView} />
               <Route component={ErrorView} />
             </Switch>
           </>
